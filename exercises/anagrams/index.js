726500d6aa6884a5c018b3803484fc6e314d52f1
Howard <howardguoui@gmail.com>
2020-04-22 15:53:09 -0400

    sd
    sd

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   const a = buildCharMap(stringA);
+//   const b = buildCharMap(stringB);
+//   if (Object.keys(a).length !== Object.keys(b).length) {
+//     return false;
+//   }
+//   for (let element in a) {
+//     // console.log(a[element], b[element]);
+//     if (a[element] !== b[element]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// function buildCharMap(str) {
+//   const chars = {};
 
+//   // of loop string
+//   for (let char of str.replace(/[^\w]/g).toLowerCase()) {
+//     if (!chars[char]) {
+//       chars[char] = 1;
+//     } else {
+//       chars[char]++;
+//     }
+//     //   chars[char] = chars[char] + 1 || 1;
+//   }
+//   return chars;
+// }
+function anagrams(stringA, stringB) {
+  const a = buildCharMap(stringA);
+  const b = buildCharMap(stringB);
+  return a === b;
+}
+function buildCharMap(str) {
+  return str.replace(/[^\w]/g).toLowerCase().split('').sort().join('');
+}
 module.exports = anagrams;
