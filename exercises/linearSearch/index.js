8a1460c650aea92aa4b77c58917bba764d7fa6de
Howard <howardguoui@gmail.com>
2020-05-18 00:08:03 -0400

    naive search

--- exercises/linearSearch/index.js
+++ exercises/linearSearch/index.js
@@ -6,3 +6,15 @@ function linearSearch(arr, val) {
 }
 
 module.exports = linearSearch;
+function naiveSearch(long, short) {
+  var count = 0;
+  for (var i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+naiveSearch('lorie loled', 'lol');
