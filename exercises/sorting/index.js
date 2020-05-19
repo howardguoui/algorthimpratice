d4ae94177728a814add56465648e33e31e8ffd5a
Howard <howardguoui@gmail.com>
2020-05-18 22:00:50 -0400

    bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,56 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+function bubbleSort(arr) {}
+// function bubbleSort(arr) {
+//   var noSwaps;
+//   for (var i = arr.length; i > 0; i--) {
+//     noSwaps = true;
+//     for (var j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         var temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//         noSwaps = false;
+//       }
+//     }
+//     if (noSwaps) break;
+//   }
+//   return arr;
+// }
+// function bubbleSort(arr) {
+//   for (var i = arr.length; i > 0; i--) {
+//     for (var j = 0; j < i - 1; j++) {
+//       console.log(arr, arr[j], arr[j + 1]);
+//       if (arr[j] > arr[j + 1]) {
+//         var temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-}
+// // ES2015 Version
+// function bubbleSort(arr) {
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
 
-function selectionSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
