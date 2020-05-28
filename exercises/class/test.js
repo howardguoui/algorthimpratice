427e03f670e422bfb9a455680b3d8dcdef4f4ad1
Howard <howardguoui@gmail.com>
2020-05-27 21:29:20 -0400

    class

---
+++ exercises/class/test.js
@@ -0,0 +1,6 @@
+const Point = require('./index');
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+test('capitalizes the first letter of every word in a sentence', () => {
+  expect(Point.distance(p1, p2)).toEqual(7.0710678118654755);
+});
