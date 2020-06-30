e042c931d3891b527ef05e8e7df3e19a271d6531
Howard <howardguoui@gmail.com>
2020-06-29 22:32:02 -0400

    pratice

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -100,29 +100,29 @@ function mergeSort(arr) {
   let right = mergeSort(arr.slice(mid));
   return merge(left, right);
 }
-// function merge(arr1, arr2) {
-//   let results = [];
-//   let i = 0;
-//   let j = 0;
-//   while (i < arr1.length && j < arr2.length) {
-//     if (arr2[j] > arr1[i]) {
-//       results.push(arr1[i]);
-//       i++;
-//     } else {
-//       results.push(arr2[j]);
-//       j++;
-//     }
-//   }
-//   while (i < arr1.length) {
-//     results.push(arr1[i]);
-//     i++;
-//   }
-//   while (j < arr2.length) {
-//     results.push(arr2[j]);
-//     j++;
-//   }
-//   return results;
-// }
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
 
 // function pivot(arr, start = 0, end = arr.length - 1) {
 //   const swap = (arr, idx1, idx2) => {
