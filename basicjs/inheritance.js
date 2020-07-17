9d17a2f536f6a60d325ba89ea09cd83a8e72a75a
Howard <howardguoui@gmail.com>
2020-07-16 23:56:48 -0400

    add exercise

---
+++ basicjs/inheritance.js
@@ -0,0 +1,51 @@
+
+function Shape() {}
+function Circle() {}
+
+// Prototypical inheritance 
+// Circle.prototype = Object.create(Object.prototype);
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle; 
+
+function Rectangle(color) {
+    // To call the super constructor 
+    Shape.call(this, color);
+}
+function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = child;
+}
+// Circle.prototype.draw = () =>{
+//     console.log('draw');
+// }
+// Method overriding 
+Shape.prototype.draw = function() {}
+Circle.prototype.draw = function() {
+    // Call the base implementation 
+    Shape.prototype.draw.call(this);
+
+    // Do additional stuff here 
+}
+
+// Don't create large inheritance hierarchies. 
+// One level of inheritance is fine. 
+
+// Use mixins to combine multiple objects 
+// and implement composition in JavaScript. 
+const canEat = { 
+    eat: function() {}
+};
+
+const canWalk = {
+    walk: function() {}
+};
+
+function mixin(target, ...sources) {
+    // Copies all the properties from all the source objects 
+    // to the target object. 
+    Object.assign(target, ...sources);
+}
+
+function Person() {}
+
+mixin(Person.prototype, canEat, canWalk);
